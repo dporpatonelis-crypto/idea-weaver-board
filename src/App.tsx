210a4d50cb522e68fc7710a4e6c50eb0de93b4aa
dporpatonelis-crypto <dporpatonelis@gmail.com>
2026-09-15 22:50:20 +0300

    fix(pages): support project-site router base path

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/conclusion" element={<Conclusion />} />
@@ -27,5 +27,5 @@ const App = () => (
     </TooltipProvider>
   </QueryClientProvider>
 );
-
+ 
 export default App;
